296026918d0c61d1ed41dc8faf797f02694833b5
Merich550 <151500247+Merich550@users.noreply.github.com>
2026-03-13 13:20:15 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils import executor
 import yt_dlp
 import os
@@ -8,37 +9,40 @@ TOKEN = "8705691968:AAFsg3UVE1YRWQl7RlIhRP7v-R_Een7kBYw"
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+# MENU
+menu = ReplyKeyboardMarkup(resize_keyboard=True)
+menu.add(
+    KeyboardButton("📥 TikTok"),
+    KeyboardButton("📥 YouTube")
+)
+menu.add(
+    KeyboardButton("📥 Instagram")
+)
 
+# START
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
-    text = """
-🤖 Super Media Bot
+    await message.answer(
+        "🤖 Super Media Bot\n\n"
+        "Video yuklash uchun tugmani tanlang:",
+        reply_markup=menu
+    )
 
-📥 TikTok / YouTube / Instagram video yuklaydi
-
-Buyruqlar:
-/tiktok
-/youtube
-/instagram
-"""
-    await message.answer(text)
-
-
-@dp.message_handler(commands=["tiktok"])
+# BUTTONS
+@dp.message_handler(lambda message: message.text == "📥 TikTok")
 async def tiktok(message: types.Message):
     await message.answer("📥 TikTok link yuboring")
 
-
-@dp.message_handler(commands=["youtube"])
+@dp.message_handler(lambda message: message.text == "📥 YouTube")
 async def youtube(message: types.Message):
     await message.answer("📥 YouTube link yuboring")
 
-
-@dp.message_handler(commands=["instagram"])
+@dp.message_handler(lambda message: message.text == "📥 Instagram")
 async def instagram(message: types.Message):
     await message.answer("📥 Instagram link yuboring")
 
 
+# DOWNLOAD
 @dp.message_handler()
 async def downloader(message: types.Message):
 
@@ -59,16 +63,16 @@ async def downloader(message: types.Message):
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
 
-        file = None
+        video_file = None
 
-        for f in os.listdir():
-            if f.startswith("video"):
-                file = f
+        for file in os.listdir():
+            if file.startswith("video"):
+                video_file = file
                 break
 
-        if file:
-            await message.answer_video(open(file, "rb"))
-            os.remove(file)
+        if video_file:
+            await message.answer_video(open(video_file, "rb"))
+            os.remove(video_file)
 
     except:
         await message.answer("❌ Video yuklab bo‘lmadi")
