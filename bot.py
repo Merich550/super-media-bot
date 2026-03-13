2cecfb807e82af26a22e1234357a7efdc31dcb89
Merich550 <151500247+Merich550@users.noreply.github.com>
2026-03-13 11:06:56 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
+import yt_dlp
+import os
 
 TOKEN = "8705691968:AAFsg3UVE1YRWQl7RlIhRP7v-R_Een7kBYw"
 
@@ -38,6 +40,32 @@ async def help_cmd(message: types.Message):
 /ai - AI chat
 """
     await message.answer(text)
+
+
+@dp.message_handler(commands=["tiktok"])
+async def tiktok_cmd(message: types.Message):
+    await message.answer("📥 TikTok link yuboring")
+
+
+@dp.message_handler()
+async def download_video(message: types.Message):
+    url = message.text
+
+    if "tiktok.com" in url:
+        await message.answer("⏳ Video yuklanmoqda...")
+
+        ydl_opts = {
+            'outtmpl': 'video.%(ext)s'
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+
+        for file in os.listdir():
+            if file.startswith("video"):
+                await message.answer_video(open(file, "rb"))
+                os.remove(file)
+                break
 
 
 if __name__ == "__main__":
