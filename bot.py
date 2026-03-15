f89b7d0ec082aeb16bd508a542390f6effea88bf
Merich550 <151500247+Merich550@users.noreply.github.com>
2026-03-15 09:41:03 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,32 +6,33 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Salom! Menga qo‘shiq nomini yuboring.")
+    await update.message.reply_text("🎵 Qo‘shiq nomini yuboring")
 
-async def music(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def search_music(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.message.text
     await update.message.reply_text("⏳ Qidirilmoqda...")
 
-    ydl_opts = {
-        'format': 'bestaudio',
-        'noplaylist': True,
-        'quiet': True
-    }
-
     try:
+        ydl_opts = {
+            "quiet": True,
+            "default_search": "ytsearch1"
+        }
+
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]
-            url = info['url']
-            title = info['title']
+            info = ydl.extract_info(query, download=False)
+            video = info["entries"][0]
+
+        title = video["title"]
+        url = video["url"]
 
         await update.message.reply_audio(audio=url, title=title)
 
-    except:
+    except Exception as e:
         await update.message.reply_text("❌ Qo‘shiq topilmadi")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, music))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, search_music))
 
 app.run_polling()
